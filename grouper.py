a8776aeb7290df610e7dd5ad08184b07c796583b
Jeff Maxim <frey.maxim@gmail.com>
2020-01-23 17:11:14 -0500

    variable # of groups

--- grouper.py
+++ grouper.py
@@ -7,20 +7,26 @@ groups = [
   []
 ]
 
-def grouper():
-  with open('emails.csv') as csvfile:
-      reader = csv.reader(csvfile)
-      for row in reader:
-          email = row[0]
-          email_group = hash(email) % 4
-          groups[email_group].append(email)
+def grouper(num_of_groups):
+    groups = []
+    for i in range(0, num_of_groups):
+        groups.append([])
 
-def writer():
+    with open('emails.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            email = row[0]
+            email_group = hash(email) % num_of_groups
+            groups[email_group].append(email)
+
+    return groups
+
+def writer(groups):
     for idx, group in enumerate(groups):
         with open(f'group{idx}.csv', 'w') as csvfile:
             email_writer = csv.writer(csvfile)
             for email in group:
                 email_writer.writerow([email])
 
-grouper()
-writer()
+groups = grouper(4)
+writer(groups)
